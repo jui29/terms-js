ef6b35cf26e484cf61a5ac9723fa54e63cb03809
jui29 <jui29>
2021-03-24 03:36:23 +0600

    first commit

---
+++ assets/js/math.js
@@ -0,0 +1,170 @@
+alert("I'm not a robot")
+  document.body.style.backgroundColor="lightgray";
+   var H = document.getElementsByTagName("h1");
+   var H=document.getElementsByTagName("h1");
+   H[0].style.backgroundColor="gray";
+   H[0].style.padding="30px";
+   H[0].style.color="white";
+   H[0].innerText="Welcome to my Javascripts  Web Page"
+   H[0].style.color="red";
+   var p =document.getElementsByTagName("p");
+   p[2].style.color="red";
+   p[2].style.padding="10px";
+   p[2].style.backgroundColor="white";
+   p[2].style.fontSize="30px";
+   p[1].style.color="gray";
+   p[0].style.color="blue";
+   p[4].style.color="green";
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+//function funName (e){
+    
+    
+    //if ( e=1 ){
+        //console.log(e);
+        //return e ;
+    //} else {
+
+        //funName ( e-1)
+        //console.log(e);
+        //return e ;
+    //}
+//}
+
+
+
+//funName (10);
+
+
+
+
+
+
+
+
+//5!=5*4!
+//4!=4*3!
+
+
+//var fac=[]
+
+//function factorial (n){
+
+//if ( n==1 || n==0){
+
+   // return 1;
+//} else {
+//return n*factorial(n-1)
+
+//}
+
+//}
+
+//var newFac=factorial(3);
+
+//console.log(newFac);
+
+
+
+//var fac=[]
+
+//function factorial (n){
+
+//if ( n==1 || n==0){
+
+    //return 1;
+//} else {
+//return n*factorial(n-1)
+
+//}
+
+//}
+
+//var newFac=factorial(10);
+
+//console.log(newFac);
+
+
+
+
+
+//var fac=[]
+
+//function factorial (n){
+
+//if ( n==1 || n==0){
+
+    //return 1;
+//} else {
+//return n*factorial(n-1)
+
+//}
+
+//}
+
+//var newFac=factorial(15);
+
+//console.log(newFac);
+
+
+
+
+//var fac=[]
+//function getFirstValue(n) {
+
+	//if (n==1 || n==0){
+		//return 1;
+	//}else {
+		//return n*getFirstValue (n-1)
+	//}
+//}
+
+//var newFac=getFirstValue(3);
+//console.log(newFac);
+
+
+
+//var fac=[]
+//function getFirstValue(n) {
+    
+	//if (n==1 || n==0){
+		//return 1;
+	//}else {
+		//return n*getFirstValue (n-1)
+	//}
+//}
+
+//var newFac=getFirstValue(20);
+//console.log(newFac);
+
+
+
+
+
+
+
+
+//var fac=[]
+//function getFirstValue(n) {
+    
+	//if (n==1 || n==0){
+		//return 1;
+	//}else {
+		//return n*getFirstValue (n-1)
+	//}
+//}
+
+//var sloved=getFirstValue(16);
+//console.log(sloved);
